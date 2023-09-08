3a67faaf9051326e1b57fa7f31f0b45901e2e598
CodeLifo <125806607+Codelifo@users.noreply.github.com>
2023-09-08 08:09:38 +0600

    Delete ToDoApp directory

--- ToDoApp/first_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from first_app.models import AddTaskModel
-
-class AddTaskForm(forms.ModelForm):
-    class Meta:
-        model = AddTaskModel
-        fields = ['id','Title','description']
--- ToDoApp/first_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class AddTaskModel(models.Model):
-    Title = models.CharField(max_length=30)
-    description = models.CharField(max_length=50)
-    status = models.CharField(max_length=50,default = " incompleted")
-    
